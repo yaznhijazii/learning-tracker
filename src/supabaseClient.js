48e3a33234cf194a4c636be00683fc664ae330b9
Yazan Hijazi <yazanbrc@gmail.com>
2025-10-05 08:25:38 +0300

    Fixed environment variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// بدّل هدول بالبيانات من Supabase
-const supabaseUrl = 'https://yxeoxxocqbgzitwbcgoj.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4ZW94eG9jcWJneml0d2JjZ29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NzIzOTIsImV4cCI6MjA3NTE0ODM5Mn0.qU0IYrFhgRFpK4ycQe4L7wHy_AwiRogb0fYfLYeBY8k'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://yxeoxxocqbgzitwbcgoj.supabase.co'
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4ZW94eG9jcWJneml0d2JjZ29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NzIzOTIsImV4cCI6MjA3NTE0ODM5Mn0.qU0IYrFhgRFpK4ycQe4L7wHy_AwiRogb0fYfLYeBY8k'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
